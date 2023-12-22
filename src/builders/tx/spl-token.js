fd9c549ad5a9014ce17617e7f3b38ca37dcea7b9
dongthanhlx <dongthanhlx@gmail.com>
2023-12-22 12:09:56 +0700

    Update spl-token.js

--- src/builders/tx/spl-token.js
+++ src/builders/tx/spl-token.js
@@ -4,10 +4,14 @@ const bs58 = require('bs58');
 const splTokenBuilder = require('../address/spl-token');
 const config = require('../../config')
 const connection = new web3.Connection(config.get('SOLANA_RPC'), 'confirmed')
+const USDCTokenAddress = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+const USDTTokenAddress = 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB';
 
 module.exports = {
     async build({currency, amount, sendTo, fromKey}) {
         const sender = web3.Keypair.fromSecretKey(bs58.decode(fromKey))
+        const tokenAddress = eval(`${currency}TokenAddress`);
+        const mint = new web3.PublicKey(tokenAddress);
         
         const fromAddress = await splTokenBuilder.render({
             ownerAddress: sender.publicKey,
@@ -21,13 +25,15 @@ module.exports = {
             secret: fromKey
         })
         
-        return await splToken.transfer(
+        return await splToken.transferChecked(
             connection,
             sender,
             fromAddress.address,
+            mint,
             toAddress.address,
             sender.publicKey,
-            amount * web3.LAMPORTS_PER_SOL / 1000
+            amount * web3.LAMPORTS_PER_SOL / 1000,
+            6
         )
     }
 }
